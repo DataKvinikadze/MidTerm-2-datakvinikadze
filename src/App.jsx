5018f9903fc12e17ea678d1c1c1f08372bb33867
DataKvinikadze <datakvinikadze07@gmail.com>
2024-10-07 22:31:00 +0400

    davaleba 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from "react";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [inputValues, setInputValues] = useState([]);
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+    console.log(inputValue);
+  };
+  const handleClick = (event) => {
+    event.preventDefault();
+    setInputValues((prev) => [...prev, inputValue]);
+    console.log(inputValues);
+  };
+
   return (
     <>
       <h1 className="text-3xl font-bold text-center">Midterm 2</h1>
+      <div>
+        <form action="">
+          <input onChange={handleChange} type="text" />
+          <button type="" onClick={handleClick}>
+            Add
+          </button>
+        </form>
+      </div>
     </>
   );
 }
